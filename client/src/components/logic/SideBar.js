96573b55c7919d0bcc5e86ba1f94f441f9567478
nhan295 <tranthihongnhan295@gmail.com>
2025-07-18 13:31:30 +0700

    feat: Add vue-multiselect dependency and update related components for tournament management

--- client/src/components/logic/SideBar.js
+++ client/src/components/logic/SideBar.js
@@ -1,4 +1,8 @@
+// SideBar.js
 import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const router = useRoute();
+
 export default {
   name: "SideBar",
   props: {
@@ -9,13 +13,46 @@ export default {
   },
   data() {
     return {
-      currentRoute: "",
+      currentRoute: "dashboard", // Set default route
+      isTournamentDropdownOpen: false,
     };
   },
+  
+  computed: {
+    isTournamentActive() {
+      return this.currentRoute === 'tournaments-parent';
+    },
+    
+    hasActiveTournamentChild() {
+      return ['tournaments', 'create-tournament', 'tournament-schedule', 'tournament-standings', 'tournament-results'].includes(this.currentRoute);
+    }
+  },
+  
   methods: {
     setActive(route) {
       this.currentRoute = route;
+      
+      // Auto-expand tournament dropdown if selecting a tournament-related route
+      if (this.hasActiveTournamentChild) {
+        this.isTournamentDropdownOpen = true;
+      }
     },
+    
+    toggleTournamentDropdown() {
+      this.isTournamentDropdownOpen = !this.isTournamentDropdownOpen;
+      
+      // Don't set parent as active when just toggling dropdown
+      // Parent will only be active if explicitly clicked via setActive()
+    },
+  },
+  
+  watch: {
+    // Watch for sidebar open/close to control dropdown
+    isSidebarOpen(newVal) {
+      if (!newVal) {
+        this.isTournamentDropdownOpen = false;
+      }
+    }
   },
 
   onMounted() {
@@ -24,5 +61,10 @@ export default {
     link.href =
       "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css";
     document.head.appendChild(link);
+    
+    // Auto-expand tournament dropdown if currently on a tournament page
+    if (this.hasActiveTournamentChild) {
+      this.isTournamentDropdownOpen = true;
+    }
   },
 };
