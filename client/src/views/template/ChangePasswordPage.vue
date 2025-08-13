a6142e02d2e3d76e811617be7a05cbddcca4ccc8
nhan295 <tranthihongnhan295@gmail.com>
2025-08-13 14:50:54 +0700

    feat: add Change Password page with styles and form functionality

---
+++ client/src/views/template/ChangePasswordPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="change-pass-form">
+        <div class="form-header">
+            <h2>Change Password</h2>
+        </div>
+        <form class="form-input" >
+            <label>Current password</label>
+            <input type="password" name="" id="">
+
+            <label>Password</label>
+            <input type="password" name="" id="">
+
+            <label>Comfirm password</label>
+            <input type="password" name="" id="">
+
+            <button type="submit" class="change-pass-btn">Update Password</button>
+        </form>
+    </div>
+</template>
+<style scoped src="../style/ChangePasswordPage.css"></style>
