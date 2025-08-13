5b524cc4a4cee668be447e2fb6da115c63dc409f
nhan295 <tranthihongnhan295@gmail.com>
2025-08-13 14:50:25 +0700

    feat: add routing for Change Password and All Tournaments pages in sidebar

--- client/src/router.js
+++ client/src/router.js
@@ -3,13 +3,17 @@ import LoginPage from './views/template/LoginPage.vue';
 import HomePage from './views/template/HomePage.vue';
 import Layout from './components/template/Layout.vue';
 import NewTourPage from './views/template/NewTourPage.vue';
+import ChangePasswordPage from './views/template/ChangePasswordPage.vue';
+import AllTourPage from './views/template/AllTourPage.vue';
 
 const routes = [
     {path: '', component: LoginPage},
     {path: '/', component: Layout,
         children:[
             {path: 'dashboard',component: HomePage},
-            {path: 'create-tournament',component: NewTourPage}
+            {path: 'create-tournament',component: NewTourPage},
+            {path: 'change-password', component: ChangePasswordPage},
+            {path: 'all-tournaments', component: AllTourPage}
         ]
     },
     
