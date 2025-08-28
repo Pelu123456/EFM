bd822b78cf0ac901dc1e5aaab221311bc7e44d81
KhanhVinh <khanhvinhb2@gmail.com>
2025-08-28 09:24:47 +0700

    roll back

--- sever/app/Providers/AppServiceProvider.php
+++ sever/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Contracts\BaseRepositoryInterface;
+use App\Repositories\Eloquent\BaseRepository;
+use App\Services\Contracts\BaseServiceInterface;
+use App\Services\Implementations\BaseService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(BaseServiceInterface::class, BaseService::class);
     }
 
     /**
